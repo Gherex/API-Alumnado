647408ce12c6dfffee4508f699da9707acab23b8
Gherex <germilagger@gmail.com>
2025-01-03 11:30:01 -0300

    Cambios en target, test sin fallos

--- alumnado/src/test/java/com/gherex/Alumnado/AlumnadoApplicationTests.java
+++ alumnado/src/test/java/com/gherex/Alumnado/AlumnadoApplicationTests.java
@@ -2,8 +2,14 @@ package com.gherex.Alumnado;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:mysql://localhost:3306/alumnado",
+		"spring.datasource.username=root",
+		"spring.datasource.password=imreg091194"
+})
 class AlumnadoApplicationTests {
 
 	@Test
